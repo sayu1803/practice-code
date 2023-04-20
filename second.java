47d95aa8632b5f47d70260a568015a5174e44fe7
sayu1803 <sayantanrabi.03@gmail.com>
2023-04-20 13:40:03 +0530

    Create second.java
    fibonacci  series code in java

---
+++ second.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+import javax.sql.rowset.spi.SyncResolver;
+
+public class second{
+    public static void printFib(int a, int b, int n){
+        if(n==0){
+            return;
+        }
+        int c = a + b ;
+        System.out.println(c);
+        printFib(b, c, n-1);
+
+    }
+    public static void main(String args[]){
+        int a = 0 ; int b=1;
+        System.out.println(a);
+        System.out.println(b);
+        int n=20;
+        printFib(a, b, n-2);
+        
+        
+    }
+}
